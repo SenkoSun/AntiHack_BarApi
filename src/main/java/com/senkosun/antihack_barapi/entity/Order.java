939586d7e05fb84d52aadbfdabba0fbc5f07537c
SenkoSun <212nazik212@gmail.com>
2026-05-05 19:42:31 +0500

    add Entity order

---
+++ src/main/java/com/senkosun/antihack_barapi/entity/Order.java
@@ -0,0 +1,45 @@
+package com.senkosun.antihack_barapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "drink_name", nullable = false)
+    private String drinkName;
+
+    @Column(nullable = false)
+    private Integer price;
+
+    @Column(nullable = false)
+    private String method;  // "order" или "mix"
+
+
+    // Хелпер для создания заказа
+    public static Order createOrder(User user, String drinkName, Integer price, String method) {
+        return Order.builder()
+                .user(user)
+                .drinkName(drinkName)
+                .price(price)
+                .method(method)
+                .build();
+    }
+}
