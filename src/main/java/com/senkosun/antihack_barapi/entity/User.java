8b73da37065ac57dacd965d6c6f2c8997dec7311
SenkoSun <212nazik212@gmail.com>
2026-05-05 19:36:30 +0500

    add lombok and Entity user

---
+++ src/main/java/com/senkosun/antihack_barapi/entity/User.java
@@ -0,0 +1,39 @@
+package com.senkosun.antihack_barapi.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String token;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Integer balance = 100;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private String rank = "Новичок";
+
+
+    public boolean deductBalance(int amount) {
+        if (this.balance >= amount) {
+            this.balance -= amount;
+            return true;
+        }
+        return false;
+    }
+}
