bbac90b29626072ba8da61b89fadb3d47e04dd7a
SenkoSun <212nazik212@gmail.com>
2026-05-07 10:59:49 +0500

    add a func for work in BarService

--- src/main/java/com/senkosun/antihack_barapi/service/BarService.java
+++ src/main/java/com/senkosun/antihack_barapi/service/BarService.java
@@ -1,11 +1,17 @@
 package com.senkosun.antihack_barapi.service;
 
+import com.senkosun.antihack_barapi.dto.response.MenuResponse;
 import com.senkosun.antihack_barapi.entity.Bar;
 import com.senkosun.antihack_barapi.entity.User;
+import com.senkosun.antihack_barapi.enums.Drink;
+
+import java.util.List;
 
 public interface BarService {
     Bar Menu();
     Bar Tip();
 
     Bar getBarByUser(User user);
+    List<MenuResponse.DrinkItem> getDrinks();
+    MenuResponse.DrinkItem toDrinkItem(Drink drink);
 }
--- src/main/java/com/senkosun/antihack_barapi/service/BarServiceImpl.java
+++ src/main/java/com/senkosun/antihack_barapi/service/BarServiceImpl.java
@@ -1,11 +1,18 @@
 package com.senkosun.antihack_barapi.service;
 
+import com.senkosun.antihack_barapi.dto.response.MenuResponse;
 import com.senkosun.antihack_barapi.entity.Bar;
 import com.senkosun.antihack_barapi.entity.User;
+import com.senkosun.antihack_barapi.enums.Drink;
+import com.senkosun.antihack_barapi.enums.Ingredient;
 import com.senkosun.antihack_barapi.repository.BarRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -28,5 +35,25 @@ public class BarServiceImpl implements BarService{
     public Bar getBarByUser(User user) {
         return barRepository.findByUser(user)
                 .orElseThrow(() -> new RuntimeException("Бар не найден для пользователя id=" + user.getId()));
+    }
+
+    @Override
+    public List<MenuResponse.DrinkItem> getDrinks() {
+        return Arrays.stream(Drink.values())
+                .map(this::toDrinkItem)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public MenuResponse.DrinkItem toDrinkItem(Drink drink) {
+        return MenuResponse.DrinkItem.builder()
+                .name(drink.getDisplayName())
+                .price(drink.getPrice())
+                .ingredients(
+                        drink.getIngredients().stream()
+                                .map(Ingredient::getDisplayName)
+                                .collect(Collectors.toList())
+                )
+                .build();
     }
 }
